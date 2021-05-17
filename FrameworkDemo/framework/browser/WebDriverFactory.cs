65a5ac3a4972ef2bc056e1ef6b43ec7686d55977
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2021-05-17 22:31:40 +0300

    add with ignore

---
+++ FrameworkDemo/framework/browser/WebDriverFactory.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkDemo.framework.browser
+{
+    class WebDriverFactory
+    {
+        public static IWebDriver GetWorkingDriver (BrowserType type)
+        {
+            IWebDriver driver = null;
+            switch (type)
+            {
+                case BrowserType.Chrome:
+                    driver = new ChromeDriver();
+                    break;
+                case BrowserType.Firefox:
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    throw new ArgumentException(String.Format("{0} is not valid browser", type));
+            }
+            driver.Manage().Window.Maximize();
+            return driver;
+        }
+    }
+}
---
+++ FrameworkDemo/tests/BaseTest.cs
@@ -0,0 +1,34 @@
+﻿using FrameworkDemo.framework.browser;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+namespace FrameworkDemo.tests
+{
+    class BaseTest
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = Browser.GetInstance().GetDriver();
+        }
+
+        [Test]
+        public void BaseTestMethod()
+        {
+
+            driver.Navigate().GoToUrl("http://google.com/");
+            Console.WriteLine("THE TITLE SHOULD BE......." + driver.Title);
+            Assert.That(driver.Title == "Google");
+             
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
