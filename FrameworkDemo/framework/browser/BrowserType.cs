6d9cb815ce0266670c991a0c38a4b2ed192c52b8
Aliaksandr Ladyhin <aliaksandr_ladyhin@epam.com>
2021-05-17 22:19:13 +0300

    add project to github

---
+++ FrameworkDemo/framework/browser/BrowserType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkDemo.framework.browser
+{
+    enum BrowserType
+    {
+     Chrome,
+     Firefox
+    }
+}
